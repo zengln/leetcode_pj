3b3fdaab76839c97ed0b56952ce6f5132e3a1a71
zengln <zengln@zln.com>
2020-07-03 09:05:40 +0800

    二进制求和plus

--- 二进制求和.py
+++ 二进制求和.py
@@ -18,13 +18,49 @@
 '''
 
 
-def addBinary(a:str, b:str) -> str:
+def addBinary1(a:str, b:str) -> str:
     # return str(bin((int(a, 2) + int(b, 2))))[2:]
     # return bin((int(a, 2) + int(b, 2)))[2:]
     return '{0:b}'.format(int(a, 2) + int(b, 2))
 
 
-a = "11"
-b = "1"
+def addBinary(a:str,b:str) -> str:
+    if a.__len__() < b.__len__() :
+        min_length = a.__len__()
+    else:
+        min_length = b.__len__()
+
+    carry = 0
+    num = ''
+
+    # 按照最短的数据长度, 低位相加
+    for i in range(min_length -1, -1, -1):
+        sum = carry + int(a[i]) + int(b[i])
+        num += str(sum % 2)
+        carry = sum // 2
+
+    # 如果 a 更长, a 继续进行运算
+    if a.__len__() - min_length > 0:
+        for i in range(0, a.__len__() - min_length):
+            sum = carry + int(a[i])
+            carry = sum // 2
+            num += str(sum % 2)
+    # 如果 b 更长, b 继续进行运算
+    if b.__len__() - min_length > 0 :
+        for i in range(0, b.__len__() - min_length):
+            sum = carry + int(b[i])
+            carry = sum // 2
+            num += str(sum % 2)
+
+    # 最终进位非0, 进1
+    if carry == 1:
+        num += '1'
+
+    return ''.join(reversed(num))
+
+
+
+a = "1010"
+b = "1011"
 print(addBinary(a, b))
 
