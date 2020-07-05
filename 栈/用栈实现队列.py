f28fb59d467822a4fa0c0aa6bf41a8bba8f7cf1f
zengln <zengln@zln.com>
2020-07-05 14:46:45 +0800

    用栈实现队列

---
+++ 栈/用栈实现队列.py
@@ -0,0 +1,81 @@
+# -*- coding:utf-8 -*-
+# @Time    : 2020/7/5 14:46
+# @Author  : zengln
+# @File    : 用栈实现队列.py
+
+# 使用栈实现队列的下列操作：
+#
+#
+#  push(x) -- 将一个元素放入队列的尾部。
+#  pop() -- 从队列首部移除元素。
+#  peek() -- 返回队列首部的元素。
+#  empty() -- 返回队列是否为空。
+#
+#
+#
+#
+#  示例:
+#
+#  MyQueue queue = new MyQueue();
+#
+# queue.push(1);
+# queue.push(2);
+# queue.peek();  // 返回 1
+# queue.pop();   // 返回 1
+# queue.empty(); // 返回 false
+#
+#
+#
+#  说明:
+#
+#
+#  你只能使用标准的栈操作 -- 也就是只有 push to top, peek/pop from top, size, 和 is empty 操作是合法的。
+#
+#  你所使用的语言也许不支持栈。你可以使用 list 或者 deque（双端队列）来模拟一个栈，只要是标准的栈操作即可。
+#  假设所有操作都是有效的 （例如，一个空的队列不会调用 pop 或者 peek 操作）。
+#
+class MyQueue:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.queue = []
+
+
+    def push(self, x: int) -> None:
+        """
+        Push element x to the back of queue.
+        """
+        tmp_queue = []
+        size = len(self.queue)
+        for i in range(size):
+            tmp_queue.append(self.queue.pop())
+
+        self.queue = [x]
+        for i in range(size):
+            self.queue.append(tmp_queue.pop())
+
+
+
+    def pop(self) -> int:
+        """
+        Removes the element from in front of queue and returns that element.
+        """
+        return self.queue.pop()
+
+
+    def peek(self) -> int:
+        """
+        Get the front element.
+        """
+        return self.queue[-1]
+
+
+    def empty(self) -> bool:
+        """
+        Returns whether the queue is empty.
+        """
+        return not self.queue
+
+
