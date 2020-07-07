a9dc76a436830ddd39f8d80324ce3ec07e03129d
zengln <zengln@zln.com>
2020-07-07 20:15:13 +0800

    删除字符串中所有相邻重复项plus

--- 栈/删除字符串中所有相邻重复项.py
+++ 栈/删除字符串中所有相邻重复项.py
@@ -28,7 +28,7 @@
 #  1 <= S.length <= 20000
 #  S 仅由小写英文字母组成。
 
-class Solution:
+class Solution1:
     def removeDuplicates(self, S: str) -> str:
         stack = []
         for x in S:
@@ -43,6 +43,17 @@ class Solution:
 
         return "".join(stack)
 
+
+class Solution:
+    def removeDuplicates(self, S: str) -> str:
+        stack = []
+        for x in S:
+            if stack and stack[-1] == x:
+                stack.pop()
+            else:
+                stack.append(x)
+
+        return "".join(stack)
 
 s = Solution()
 print(s.removeDuplicates("abbaca"))
