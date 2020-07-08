8654cfdf51b00dbd6685d3424b43927119a2eddc
zengln <zengln@zln.com>
2020-07-08 19:44:11 +0800

    二叉树的中序遍历,迭代版

---
+++ 栈/二叉树的中序遍历.py
@@ -0,0 +1,48 @@
+# -*- coding:utf-8 -*-
+# @Time    : 2020/7/8 19:41
+# @Author  : zengln
+# @File    : 二叉树的中序遍历.py
+
+# 给定一个二叉树，返回它的中序 遍历。
+#
+#  示例:
+#
+#  输入: [1,null,2,3]
+#    1
+#     \
+#      2
+#     /
+#    3
+#
+# 输出: [1,3,2]
+#
+#  进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+from typing import List
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        if not root:
+            return []
+
+        result = []
+
+        def help(root, result):
+            if root.left != None:
+                help(root.left, result)
+
+            result.append(root.val)
+
+            if root.right != None:
+                help(root.right, result)
+
+            return result
+        return help(root, result)
+
+
