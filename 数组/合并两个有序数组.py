03e88a1187a45ba4d343ff6b527b35551297ae2a
zengln <zengln@zln.com>
2021-01-04 19:38:03 +0800

    第二版本解法

--- 数组/合并两个有序数组.py
+++ 数组/合并两个有序数组.py
@@ -29,7 +29,7 @@
 #  nums1.length == m + n
 #  nums2.length == n
 
-
+# 第一版
 def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
 
     temp = []
@@ -55,3 +55,72 @@ def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
 
     for index in range(len(temp)):
         nums1[index] = temp[index]
+
+#第二版，减少一次for循环,减少temp变量占用内存
+def merge2(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        temp = nums1[:m]
+        nums1_index = 0
+        nums2_index = 0
+        index = 0
+        while nums1_index < m and nums2_index < n:
+            if temp[nums1_index] < nums2[nums2_index]:
+                nums1[index] = temp[nums1_index]
+                nums1_index += 1
+            elif temp[nums1_index] > nums2[nums2_index]:
+                nums1[index] = nums2[nums2_index]
+                nums2_index += 1
+            else:
+                nums1[index] = nums2[nums2_index]
+                index += 1
+                nums1[index] = temp[nums1_index]
+                nums1_index += 1
+                nums2_index += 1
+            index += 1
+
+        if nums1_index < m:
+            for temp_index in range(nums1_index, m):
+                nums1[index] = temp[temp_index]
+                index += 1
+        elif nums2_index < n:
+            for temp_index in range(nums2_index, n):
+                nums1[index] = nums2[temp_index]
+                index += 1
+
+
+# 大佬同思路做法
+# 清空nums1,后续直接append，这样可以不用记录当前需要替换nums1的哪个数据
+# 相等的场景不额外使用一个分支
+
+def merge(self, nums1, m, nums2, n):
+    """
+    :type nums1: List[int]
+    :type m: int
+    :type nums2: List[int]
+    :type n: int
+    :rtype: void Do not return anything, modify nums1 in-place instead.
+    """
+    # Make a copy of nums1.
+    nums1_copy = nums1[:m]
+    nums1[:] = []
+
+    # Two get pointers for nums1_copy and nums2.
+    p1 = 0
+    p2 = 0
+
+    # Compare elements from nums1_copy and nums2
+    # and add the smallest one into nums1.
+    while p1 < m and p2 < n:
+        if nums1_copy[p1] < nums2[p2]:
+            nums1.append(nums1_copy[p1])
+            p1 += 1
+        else:
+            nums1.append(nums2[p2])
+            p2 += 1
+
+    # if there are still elements to add
+    if p1 < m:
+        nums1[p1 + p2:] = nums1_copy[p1:]
+    if p2 < n:
+        nums1[p1 + p2:] = nums2[p2:]
+
+
