87dd592894978f8f60e0e29b8e52a3efde7f94c9
zengln <zengln@zln.com>
2021-02-24 09:32:38 +0800

    冒泡排序

--- 基础排序/排序数组.py
+++ 基础排序/排序数组.py
@@ -53,3 +53,16 @@ class Solution:
                     result.insert(left, i)
 
         return result
+
+# 冒泡排序
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(len(nums) - i - 1):
+                if nums[j] > nums[j + 1]:
+                    temp = nums[j]
+                    nums[j] = nums[j + 1]
+                    nums[j + 1] = temp
+
+        return nums
+
