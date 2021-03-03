003734c65ad48bd535d7dfe15c15c5426b335c36
zengln <zengln@zln.com>
2021-03-03 10:28:23 +0800

    快排思路，时间复杂度N

--- 基础排序/移动零.py
+++ 基础排序/移动零.py
@@ -43,3 +43,15 @@ class Solution:
         for index in range(num, len(nums)):
             nums[index] = 0
 
+
+# 快排思路
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        j = 0
+        for i in range(len(nums)):
+            if nums[i]:
+                nums[i], nums[j] = nums[j], nums[i]
+                j += 1
