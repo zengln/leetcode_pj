fd9b90970a08e8e2283a46d4498cd3a310c7de90
zengln <zengln@zln.com>
2021-07-23 20:13:21 +0800

    休闲题

---
+++ 其他/加一.py
@@ -0,0 +1,57 @@
+# -*- coding:utf-8 -*-
+# @Time : 2021/7/23 20:13 
+# @Author : zengln
+# @File : 加一.py 
+# 给定一个由 整数 组成的 非空 数组所表示的非负整数，在该数的基础上加一。
+#
+#  最高位数字存放在数组的首位， 数组中每个元素只存储单个数字。
+#
+#  你可以假设除了整数 0 之外，这个整数不会以零开头。
+#
+#
+#
+#  示例 1：
+#
+#
+# 输入：digits = [1,2,3]
+# 输出：[1,2,4]
+# 解释：输入数组表示数字 123。
+#
+#
+#  示例 2：
+#
+#
+# 输入：digits = [4,3,2,1]
+# 输出：[4,3,2,2]
+# 解释：输入数组表示数字 4321。
+#
+#
+#  示例 3：
+#
+#
+# 输入：digits = [0]
+# 输出：[1]
+#
+#
+#
+#
+#  提示：
+#
+#
+#  1 <= digits.length <= 100
+#  0 <= digits[i] <= 9
+#
+#  Related Topics 数组 数学
+
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        for index in range(len(digits)-1, -1, -1):
+            if digits[index] + 1 < 10:
+                digits[index] += 1
+                return digits
+
+            digits[index] = 0
+        digits.insert(0, 1)
+        return digits
+
