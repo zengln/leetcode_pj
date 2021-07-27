f2868708615db806cd74621679a2854459e4c98b
zengln <zengln@zln.com>
2021-07-27 14:38:20 +0800

    迭代

--- 动态规划/斐波那契数.py
+++ 动态规划/斐波那契数.py
@@ -54,3 +54,12 @@ class Solution:
             result.append(result[i-1] + result[i-2])
         return result[n]
 
+
+class Solution:
+    def fib(self, n: int) -> int:
+        def f(n):
+            if n == 0 or n == 1:
+                return n
+            return f(n-1) + f(n-2)
+
+        return f(n)
