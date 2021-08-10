45408e33b51a997c2a4c7d730bd41336348aad62
zengln <zengln@zln.com>
2021-08-10 08:40:27 +0800

    过了一段时间再做一次

--- 动态规划/杨辉三角.py
+++ 动态规划/杨辉三角.py
@@ -41,3 +41,15 @@ class Solution:
         return result
 
 
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        result = []
+        for i in range(numRows):
+            temp = []
+            for j in range(i+1):
+                if j in [0, i]:
+                    temp.append(1)
+                else:
+                    temp.append(result[i-1][j]+result[i-1][j-1])
+            result.append(temp)
+        return result
